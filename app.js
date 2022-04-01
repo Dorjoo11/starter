b1c12a60f2fef3e3f0011a3ce85aa2d9ec0504cb
doogii <ddoogii1117@gmail.com>
2022-04-01 17:53:51 +0800

    Eventlistneruudiig holboj ugluu

---
+++ app.js
@@ -0,0 +1,30 @@
+//delgets ajillah controller
+var uiController = (function(){
+    
+})();
+var finController = (function(){
+
+})();
+var appController = (function(uiController,finController){
+    var ctrlAddItem = function(){
+        //oruulah ugugdliig delgetsees olj avna
+        
+        //olj avsan ugugdluudee sanhuugiin kontrollert hiij tend ni hadgalna
+
+        //ugugdluudiig web deer tohiroh hesegt gargana
+
+        //tusuviig tootsoolno
+            
+    };
+    document.addEventListener('keypress',function(event){
+        if ( event.keyCode == 13 || event.which == 13) {
+        ctrlAddItem();
+        }
+    }
+    );
+    document.querySelector(".add__btn").addEventListener("click",function checkbutton(){ 
+        ctrlAddItem();
+        });
+    document.addEventListener
+})(uiController,uiController);
+
